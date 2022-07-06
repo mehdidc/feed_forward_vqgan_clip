1fb1806e0ea8835cbdad5e11bb8689b409610cf0
Mehdi Cherti <m.cherti@fz-juelich.de>
2022-07-06 15:41:04 +0200

    replicate: simplify by loading all models and going back and forth between CPU and GPU

--- predict.py
+++ predict.py
@@ -8,7 +8,6 @@ import torch
 from PIL import Image
 import clip
 import torchvision
-from functools import lru_cache
 from main import load_vqgan_model, CLIP_DIM, clamp_with_grad, synth, load_clip_model, load_model, load_prior_model
 
 MODELS = [
@@ -41,30 +40,37 @@ GRID_SIZES = [
     "8x8",
 ]
 
-@lru_cache(maxsize=3)
-def cached_load_model(path, device):
-    return load_model(path).eval().requires_grad_(False).to(device)
-
-@lru_cache(maxsize=2)
-def cached_load_clip_model(clip_model, model_path, device):
-    return load_clip_model(clip_model, path=model_path).eval().requires_grad_(False).to(device)
-
-@lru_cache(maxsize=2)
-def cached_load_prior_model(path, device):
-    return load_prior_model(path).eval().requires_grad_(False).to(device)
-
-@lru_cache(maxsize=1)
-def cached_load_vqgan_model(vqgan_config, vqgan_checkpoint, device):
-    vq = load_vqgan_model(vqgan_config, vqgan_checkpoint).to(device)
-    z_min = vq.quantize.embedding.weight.min(dim=0).values[None, :, None, None]
-    z_max = vq.quantize.embedding.weight.max(dim=0).values[None, :, None, None]
-    return vq, z_min, z_max
-
 class Predictor(cog.BasePredictor):
 
     def setup(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-   
+        self.nets = {
+            model_path: load_model(model_path).cpu()
+            for model_path in MODELS
+        }
+        self.priors = {}
+        self.perceptors = {}
+        self.vqgans = {}
+        for path, net in self.nets.items():
+            config = net.config
+            vqgan_config = config.vqgan_config
+            vqgan_checkpoint = config.vqgan_checkpoint
+            clip_model = config.clip_model
+            clip_model_path = config.get("clip_model_path")
+            # Load CLIP model if not already done 
+            if (clip_model, clip_model_path) not in self.perceptors:
+                perceptor = load_clip_model(clip_model, path=clip_model_path).eval().requires_grad_(False).to(self.device)
+                self.perceptors[(clip_model, clip_model_path)] = perceptor
+            # Load VQGAN model if not already done
+            if (vqgan_config, vqgan_checkpoint) not in self.vqgans:
+                model = load_vqgan_model(vqgan_config, vqgan_checkpoint).to(self.device)
+                z_min = model.quantize.embedding.weight.min(dim=0).values[None, :, None, None]
+                z_max = model.quantize.embedding.weight.max(dim=0).values[None, :, None, None]
+                self.vqgans[(vqgan_config, vqgan_checkpoint)] = model, z_min, z_max
+            # Load PRIOR model if not already done
+            if PRIOR_MODEL[path] not in self.priors:
+                self.priors[PRIOR_MODEL[path]] = load_prior_model(PRIOR_MODEL[path]).cpu()
+
     def predict(
         self, 
         prompt:str=cog.Input(description="prompt for generating image"), 
@@ -77,7 +83,10 @@ class Predictor(cog.BasePredictor):
         random.seed(seed)
         if model == "random":
             model = random.choice(list(self.nets.keys()))
-        net = cached_load_model(model, self.device)
+        net = self.nets[model]
+
+        net.to(self.device)
+
         config = net.config
         clip_model = config.clip_model
         clip_model_path = config.get("clip_model_path")
@@ -87,10 +96,11 @@ class Predictor(cog.BasePredictor):
         grid_size_h = int(grid_size_h)
         grid_size_w = int(grid_size_w)
         toks = clip.tokenize([prompt], truncate=True)
-        perceptor = cached_load_clip_model(clip_model, clip_model_path, self.device)
-        vqgan, z_min, z_max = cached_load_vqgan_model(vqgan_config, vqgan_checkpoint, self.device)
+        perceptor = self.perceptors[(clip_model, clip_model_path)]
+        vqgan, z_min, z_max = self.vqgans[(vqgan_config, vqgan_checkpoint)]
         if prior:
-            prior_model = cached_load_prior_model(PRIOR_MODEL[model], self.device)
+            prior_model = self.priors[PRIOR_MODEL[model]]
+            prior_model.to(self.device)
         with torch.no_grad():
             H = perceptor.encode_text(toks.to(self.device)).float()
             H = H.repeat(grid_size_h*grid_size_w, 1)
@@ -104,4 +114,7 @@ class Predictor(cog.BasePredictor):
         grid = torchvision.utils.make_grid(xr.cpu(), nrow=grid_size_h)
         out_path = cog.Path(tempfile.mkdtemp()) / "out.png"
         torchvision.transforms.functional.to_pil_image(grid).save(out_path)
+        net.cpu()
+        if prior:
+            prior_model.cpu()
         return out_path
